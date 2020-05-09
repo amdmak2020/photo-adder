ae24d21cb770390a85f2a1fb27aad0f248607198
ahmed <ahmedhussein0114278@gmail.com>
2020-05-09 19:15:35 +0300

    my favoirate by me ?!?!?!

--- photo_adder.js
+++ photo_adder.js
@@ -3,6 +3,7 @@ function alo () {
     var con = document.getElementById('cat');
     ne.id = 'remov'
     ne.src = "https://media.tenor.com/images/c50ca435dffdb837914e7cb32c1e7edf/tenor.gif";
+    ne.alt = "amdmak"
     con.appendChild(ne)
 }
 function remo() {
